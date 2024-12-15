fb3cd89e545d41dccc4de709c4944e28e10730d6
Rashmi Ranjan Parida <>
2024-12-15 16:36:01 -0500

    white background of logo

--- src/app/ui/sidenav/sidenav.tsx
+++ src/app/ui/sidenav/sidenav.tsx
@@ -35,7 +35,7 @@ export default function SideNav() {
       } h-full flex-col px-3 py-4 transition-all duration-300 relative`}
     >
       <Link
-        className={`mb-2 flex h-30 items-end justify-start rounded-md bg-blue-200 p-4 ${
+        className={`mb-2 flex h-30 items-end justify-start rounded-md bg-white p-4 ${
           isCollapsed ? "justify-center" : ""
         }`}
         href="/"
