7075ee01a5b1343e321a8cbdd765d84edad3a11a
Rashmi Ranjan Parida <>
2025-01-14 10:59:24 -0500

    small change for logo

--- src/app/ui/logo.tsx
+++ src/app/ui/logo.tsx
@@ -1,10 +1,17 @@
 import { lusitana, montserrat, roboto } from '@/app/ui/fonts';
 import Image from 'next/image';
 import { useSession } from "next-auth/react";
+import { useState } from 'react';
 
 export default function Logo() {
   const { data: session, status } = useSession();
   const orgId = session?.user?.secondaryOrgId? session?.user?.secondaryOrgId : session?.user?.primaryOrgId;
+
+  const [imageSrc, setImageSrc] = useState(`/images/${orgId}.pnga`);
+
+  const handleError = () => {
+    setImageSrc(`/images/logo-blue.png`);
+  };
 
   // console.log(`printing orgid in logo`, session?.user);
   return (
@@ -14,11 +21,12 @@ export default function Logo() {
     <div className="flex flex-col items-center">
       <Image
         alt="CompanyLogo"
-        src={`/images/${orgId}.png`}
+        src={imageSrc}
         sizes="100vw"
         width={100}
         height={100}
         className="object-contain"
+        onError={handleError}
       />
     </div>
     // </div>
